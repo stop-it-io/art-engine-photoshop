c503b89765a8a9465c35c3ba4652ed7c4b11215a
Stop-It.io Team <info@stop-it.io>
2022-09-12 11:39:50 +0200

    Modified HashLips generate.js script with additional features for Hedera and Stop-It.io purposes.

--- README.md
+++ README.md
@@ -1,5 +1,92 @@
-# hashlips_art_engine_ps_script
-This script allows you to generate art works right from photoshop.
+# NFT art engine for Photoshop
+Originally forked from [HashLips](hashlips_art_engine_ps_script) where all credit is due for the original source code.
 
+This script allows you to generate art works right from photoshop. 
+It has been heavily modified for needs of our own collections.
 
-Keep in mind this script is experimental and still in development, but you can try it out in the meantime.
+**Major changes derived from the original script**:
+- changes to metadata structure to be compliant with Hedera HIP-412,
+- made generation script more efficient in general,
+- added documentation and comments for the script internals,
+- added additional configuration options:
+  - ability to define whether duplicates are allowed,
+  - ability to ignore photoshop layers while generating and randomizing,
+  - ability to generate a CSV file with metadata,
+  - ability to suppress most of the prompts when generating using configuration.
+
+## Configuration options
+Configuration options can be changed in `generate.js` script, top section.
+- `ignoredLayers`: (string[])
+  - List of layers that will not be randomized during generation and their visibility will be left in initial state before starting the generation process.
+  - Note<sup>1</sup>: These must be exactly the same as name of the "group/layer" in Photoshop.
+  - Note<sup>2</sup>: Only top-level groups/layers are considered. Anything nested in groups is ignored and the script does not support it.
+- `generateCsvFileWithMetadata`: (boolean)
+  - Should CSV file with metadata be also generated? Useful for quick analysis of the collection and calculating trait distribution or checking for duplicates. File will be output to build folder.
+- `allowDuplicates`: (boolean)
+  - Can the collection contain duplicates? Be aware, in large collections and/or with collections with low number of possible trait combinations this may severely impact time needed to generate the collection.
+- `pairedLayers`: (object[])
+  - Paired layers are useful when certain groups and layers relate to each other. If the matching layer is to be visible then a related group name will also be visible. This is useful for example for weather layers - thunderstorm in the background and rain layer in the foreground.
+    - `matchLayerName`: (string)
+      - When generated layer matches then make "relatedGroupName" also visible.
+    - `relatedGroupName`: (string)
+      - Name of the related group to be made visible. Note only top-level groups are supported. All nested layers within group will be made visible.
+- `collectionMetadata`:
+  - `desiredSupply`: (number)
+    - Number of many NFTs will be generated.
+    - If present prompt for supply is suppressed.
+  - `name`: (string)
+    - Common name for all the NFTs. Note script automatically appends sequence number  `#<number>`.
+    - *Example*: "My NFT" results in: "My NFT #1"
+    - If present prompt for supply is suppressed.
+  - `description`: (string)
+    - Description, common for all the NFTs.
+    - If present prompt for supply is suppressed.
+  - `creator`: (string)
+    - Common creator for all the NFTs.
+    - If present prompt for supply is suppressed.
+
+## Rarity settings
+Same as the original from HashLips the script supports adding rarity weights of individual traits.
+For more info please see this video tutorial directly from HashLips: https://www.youtube.com/watch?v=YAexk-Qjtt0
+
+Weights to individual layers can be added when `#<number>` is appended to layer name. E.g.: "Pink hat#10".
+When weight is not specified for layer, "1" is assumed.
+
+## Final notes
+### Estimating time needed
+The script does not calculate time estimations. In case of large collections, generating may take significant amounts of time.
+Photoshop will appear frozen/not responding during the generation process. You can keep track of the progress in the build folder.
+To estimate time needed, we suggest generate a smaller sample and see how much time the sample takes. Based on that an estimation can be made.
+Please be aware that due to the nature of how duplicates are determined, generation process will gradually slow down as probability of generating non-unique combination raises.
+
+Impacting factors are:
+ - computing power of the computer and disk speed,
+ - whether duplicates are allowed,
+ - size of the picture and number of groups and layers,
+ - number of possible trait combinations.
+
+### Metadata format
+Stop-It.io operates on [Hedera](https://hedera.com) network. The metadata format is according to the specification of the Hedera network, however it may be usable for other networks as well.
+See also:
+ - https://hips.hedera.com/hip/hip-412
+
+### Editing the script
+- When making any changes to the script please be aware that Photoshop uses archaic [ECMA 262 (1999)](https://www.ecma-international.org/wp-content/uploads/ECMA-262_3rd_edition_december_1999.pdf) and many modern functions are not available.
+- Good documentation on Photoshop scripting API is available here: https://theiviaxx.github.io/photoshop-docs/Photoshop/index.html
+- When editing the script debugging functionality is provided using a log function. Set variable `loggingEnabled` to `true` and then use `log("your message");` at your discretion in the code. A log file will be created in the build folder.
+
+## Disclaimer
+Use absolutely only at your own risk. We strive to provide Hedera community with tools we develop, however everything we provide is "AS-IS".
+
+There are inherent dangers in the use of any software available for download on the Internet, and we caution you to make sure that you completely understand the potential risks before downloading any of the software.
+
+The Software and code samples available on this website are provided "**AS-IS**" without warranty of any kind, either express or implied. Use at your own risk.
+
+The use of the software and scripts downloaded on this site is done at your own discretion and risk and with agreement that you will be solely responsible for any damage to your computer system or loss of data that results from such activities. You are solely responsible for adequate protection and backup of the data and equipment used in connection with any of the software, and we will not be liable for any damages that you may suffer in connection with using, modifying or distributing any of this software. No advice or information, whether oral or written, obtained by you from us or from this website shall create any warranty for the software.
+
+We make no warranty that:
+- The software will meet your requirements.
+- The software will be uninterrupted, timely, secure or error-free.
+- The results that may be obtained from the use of the software will be effective, accurate or reliable.
+- The quality of the software will meet your expectations.
+- Any errors in the software obtained from us will be corrected.
--- generate.js
+++ generate.js
@@ -1,115 +1,291 @@
-// Simple art generator by HashLips <->
+#include
+"./lib/json2.js";
 
-#include "./lib/json2.js";
-
+/*==================== CONFIGURATION, EDIT VALUES HERE ==================== */
+var CONFIG = {
+  //List of groups/layers that will not be randomized during generation and their visibility will be left in initial state.
+  //Examples: static background layers, ground layers, typically static layers present in all pictures.
+  ignoredGroups: ["myIgnoredLayer"],
+  //Should CSV file with metadata be also generated?
+  //Useful for quick analysis of the collection and traits distribution.
+  generateCsvFileWithMetadata: true,
+  //Can the collection contain duplicates?
+  allowDuplicates: false,
+  //Paired layers are useful when certain groups and layers relate to each other.
+  //If the matching layer is to be visible then a related group name will also be visible.
+  //This is useful for example for weather layers - thunderstorm in the background and rain layer in the foreground.
+  pairedLayers: [
+    {
+      matchLayerName: "Example",
+      relatedGroupName: "example"
+    }
+  ],
+  //If present prompts will be not shown before NFT generation.
+  collectionMetadata: {
+    desiredSupply: 10,
+    name: null,
+    description: null,
+    creator: null
+  }
+};
+var logFile;
+var loggingEnabled = false; //use this if debugging is needed and then function log(message);
+/*==================== DO NOT MODIFY ANYTHING BELOW UNLESS YOU KNOW WHAT YOU ARE DOING ==================== */
 function main() {
   var continueConfirmation = confirm(
-    "You are about to use the HashLips art generator. Are you sure you want to continue?"
+      "You are about to use the HashLips art generator. Are you sure you want to continue? " +
+      "Generation may take a while depending on the collection size. For progress please see " +
+      "the build folder."
   );
+
+  var desiredSupply, name, description, creator;
 
   if (!continueConfirmation) return;
 
-  var supply = prompt("How many images do you want to generate?", "10");
+  if (!CONFIG.collectionMetadata || !CONFIG.collectionMetadata.desiredSupply) {
+    desiredSupply = prompt("How many images do you want to generate?", "10");
+  } else {
+    desiredSupply = CONFIG.collectionMetadata.desiredSupply;
+  }
+  if (!CONFIG.collectionMetadata || !CONFIG.collectionMetadata.name) {
+    name = prompt("What is the name of your collection?", "replace me");
+  } else {
+    name = CONFIG.collectionMetadata.name;
+  }
+  if (!CONFIG.collectionMetadata || !CONFIG.collectionMetadata.description) {
+    description = prompt("What is the description for your collection?", "replace me");
+  } else {
+    description = CONFIG.collectionMetadata.description;
+  }
+  if (!CONFIG.collectionMetadata || !CONFIG.collectionMetadata.creator) {
+    creator = prompt("Who is the author of the collection?", "replace me");
+  } else {
+    creator = CONFIG.collectionMetadata.creator;
+  }
 
-  var name = prompt("What is the name of your collection?", "");
-
-  var description = prompt("What is the description for your collection?", "");
-
-  alert(
-    supply +
-      " images will be generated, so sit back relax and enjoy the art being generated."
-  );
+  if (loggingEnabled === true) {
+    logFile = new File(createBuildFolder("build/logs") + "/log.txt");
+    logFile.open("w");
+    logFile.close();
+  }
 
   var groups = app.activeDocument.layerSets;
-
   resetLayers(groups);
 
-  function getRWeights(_str) {
-    var weight = Number(_str.split("#").pop());
-    if(isNaN(weight)){
-      weight = 1;
+  //Function for checking existing combinations and avoid duplicates
+  var alreadyMintedCombinations = [];
+
+  function isAlreadyMinted(traitCombination) {
+    if (CONFIG.allowDuplicates === true) {
+      return false;
     }
-    return weight;
+    for (var i = 0; i < alreadyMintedCombinations.length; i++) {
+      if (alreadyMintedCombinations[i] === traitCombination) {
+        return true;
+      }
+    }
+    return false;
   }
 
-  function cleanName(_str) {
-    return _str.split("#").shift();
+  var mintedCsvFile = null;
+
+  if (CONFIG.generateCsvFileWithMetadata) {
+    mintedCsvFile = new File(createBuildFolder("build/csv") + "/minted-nfts.csv");
+    mintedCsvFile.open("w");
+    mintedCsvFile.close();
   }
 
-  for (var h = 1; h < parseInt(supply) + 1; h++) {
-    var obj = {};
-    obj.name = name + " #" + h;
-    obj.description = description;
-    obj.image = "To be replaced";
-    obj.edition = h;
-    obj.attributes = [];
+  var groupRarityWeights = [];
+
+  /**
+   * Calculates weights for each group and their layers.
+   */
+  function calculateGroupRarityWeights() {
+    var csvHeader = "#,"; //Construct the header for CSV file
+
+    for (var i = 0; i < groups.length; i++) {
+      var weighted = {};
+      weighted.totalWeight = 0;
+      weighted.layerMap = [];
+      if (indexOf(CONFIG.ignoredGroups, getCleanLayerName(groups[i].name), null) === -1) {
+        csvHeader = csvHeader + groups[i].name + ",";
+      }
+      for (var j = 0; j < groups[i].layers.length; j++) {
+        weighted.totalWeight += getRarityWeight(groups[i].layers[j].name);
+        weighted.layerMap.push({
+          index: j,
+          name: getCleanLayerName(groups[i].layers[j].name),
+          weight: getRarityWeight(groups[i].layers[j].name)
+        });
+      }
+      groupRarityWeights.push(weighted);
+    }
+    csvHeader += "duplicates check"
+    if (CONFIG.generateCsvFileWithMetadata === true) {
+      appendToFile(mintedCsvFile, csvHeader + "\n");
+    }
+  }
+
+  calculateGroupRarityWeights();
+
+  while (alreadyMintedCombinations.length < parseInt(desiredSupply)) {
+    var metadataObj = {};
+    var nftSequenceNumber = alreadyMintedCombinations.length + 1;
+
+    metadataObj.name = name + " #" + parseInt(nftSequenceNumber);
+    metadataObj.creator = creator;
+    metadataObj.description = description;
+    metadataObj.image = "To be replaced";
+    metadataObj.type = "image/png";
+    metadataObj.format = "opensea";
+    metadataObj.attributes = [];
+
+    var generatedProperties = ""; //Stringyfied list of properties generated in this iteration.
+    var generatedPropertiesCsv = (nftSequenceNumber) + ",";
+
+    //Iterate through top-level groups of layers
     for (var i = 0; i < groups.length; i++) {
       var totalWeight = 0;
       var layerMap = [];
+      //Do not iterate through ignored (fixed) layers.
+      if (indexOf(CONFIG.ignoredGroups, getCleanLayerName(groups[i].name)) === -1) {
+        totalWeight = groupRarityWeights[i].totalWeight;
+        layerMap = groupRarityWeights[i].layerMap;
 
-      for(var j = 0; j < groups[i].layers.length; j++){
-        totalWeight += getRWeights(groups[i].layers[j].name);
-        layerMap.push({
-          index: j,
-          name: cleanName(groups[i].layers[j].name),
-          weight: getRWeights(groups[i].layers[j].name)
-        });
-      }
+        var random = Math.floor(Math.random() * totalWeight);
 
-      var ran = Math.floor(Math.random() * totalWeight);
+        (function () {
+          for (var j = 0; j < groups[i].layers.length; j++) {
+            random -= layerMap[j].weight;
+            if (random < 0) {
+              //Check whether current layer matches any from the paired list.
+              var indexOfMatched = indexOf(CONFIG.pairedLayers, getCleanLayerName(groups[i].layers[j].name), "matchLayerName");
+              if (indexOfMatched !== -1) {
+                //Find the related group index, iterate through group layers and enable all of them.
+                var relatedGroupIndex = indexOf(groups, CONFIG.pairedLayers[indexOfMatched].relatedGroupName, getCleanLayerName("name"));
 
-      (function() {
-        for(var j = 0; j < groups[i].layers.length; j++){
-          ran -= layerMap[j].weight;
-          if(ran < 0) {
-            groups[i].layers[j].visible = true;
-            obj.attributes.push({
-              trait_type: groups[i].name, 
-              value: layerMap[j].name
-            })
-            return;
+                //Set all layers within group visible
+                for (var k = 0; k < groups[relatedGroupIndex].layers.length; k++) {
+                  groups[relatedGroupIndex].layers[k].visible = true;
+                }
+              }
+
+              groups[i].layers[j].visible = true;
+              metadataObj.attributes.push({
+                trait_type: groups[i].name,
+                value: layerMap[j].name
+              })
+              generatedProperties += groups[i].name + "_" + layerMap[j].name + ":"
+              generatedPropertiesCsv += layerMap[j].name + ","
+              return;
+            }
           }
-        }
-      })();
+        })();
+      }
     }
-    saveImage(obj.edition);
-    saveMetadata(obj);
+
+    if (isAlreadyMinted(generatedProperties) === false) {
+      saveImage(nftSequenceNumber);
+      saveMetadata(metadataObj, nftSequenceNumber);
+      appendToFile(mintedCsvFile, generatedPropertiesCsv + generatedProperties + "\n");
+      alreadyMintedCombinations.push(generatedProperties);
+    } else {
+      //Do nothing, skip
+    }
     resetLayers(groups);
   }
+
   alert("Generation process is complete.");
 }
 
+/**
+ * Resets layers into initial state. Function is called before each NFT generation.
+ * @param {Array.<LayerSet>} _groups
+ */
 function resetLayers(_groups) {
+  var skippedGroups = [];
+  for (var k = 0; k < CONFIG.ignoredGroups.length; k++) {
+    log("CONFIG.pairedLayers:" + CONFIG.pairedLayers + "CONFIG.ignoredGroups[k]: " + CONFIG.ignoredGroups[k]);
+    if (indexOf(CONFIG.pairedLayers, CONFIG.ignoredGroups[k], "relatedGroupName") === -1) {
+      skippedGroups.push(CONFIG.ignoredGroups[k]);
+      log("Pushed skipped group:" + CONFIG.ignoredGroups[k]);
+    }
+  }
+
+  log("resetLayers, ignored groups: " + CONFIG.ignoredGroups);
+  log("resetLayers, var skippedGroups: " + skippedGroups.toString());
   for (var i = 0; i < _groups.length; i++) {
     _groups[i].visible = true;
+
     for (var j = 0; j < _groups[i].layers.length; j++) {
-      _groups[i].layers[j].visible = false;
+      if (indexOf(skippedGroups, getCleanLayerName(_groups[i].name), null) === -1) {
+        _groups[i].layers[j].visible = false;
+      }
     }
   }
 }
 
-function saveImage(_edition) {
-  var saveFile = new File(toFolder("build/images") + "/" + _edition + ".png");
+function log(message) {
+  if (loggingEnabled === true)
+    appendToFile(logFile, "[" + new Date().toString() + "] " + message + "\n");
+}
+
+/**
+ * Returns layer name stripped of rarity weight
+ * @param {string} layerName name of the layer with rarity
+ * @returns {string} clean layer name (part before #)
+ */
+function getCleanLayerName(layerName) {
+  return layerName.split("#").shift();
+}
+
+/**
+ * Saves an image to a file
+ * @param {string|number} sequenceNumber NFT sequence number
+ */
+function saveImage(sequenceNumber) {
+  var saveFile = new File(createBuildFolder("build/images") + "/" + sequenceNumber + ".png");
   exportOptions = new ExportOptionsSaveForWeb();
   exportOptions.format = SaveDocumentType.PNG;
   exportOptions.PNG24 = false;
   exportOptions.transparency = true;
   exportOptions.interlaced = false;
   app.activeDocument.exportDocument(
-    saveFile,
-    ExportType.SAVEFORWEB,
-    exportOptions
+      saveFile,
+      ExportType.SAVEFORWEB,
+      exportOptions
   );
 }
 
-function saveMetadata(_data) {
-  var file = new File(toFolder("build/metadata") + "/" + _data.edition + ".json");
+/**
+ * Saves metadata of an NFT into .json file
+ * @param {object} _data NFT metadata object
+ * @param {string|number} sequenceNumber NFT sequence number
+ */
+function saveMetadata(_data, sequenceNumber) {
+  var file = new File(createBuildFolder("build/metadata") + "/" + sequenceNumber + ".json");
   file.open("w");
   file.write(JSON.stringify(_data));
   file.close();
 }
 
-function toFolder(_name) {
+/**
+ * Appends line of text to a file.
+ * @param {File} file
+ * @param {string} lineToBeAppended to the file
+ */
+function appendToFile(file, lineToBeAppended) {
+  file.open("a");
+  file.write(lineToBeAppended);
+  file.close();
+}
+
+/**
+ * Creates a folder on filesystem.
+ * @param {string} _name path
+ * @returns {Folder}
+ */
+function createBuildFolder(_name) {
   var path = app.activeDocument.path;
   var folder = new Folder(path + "/" + _name);
   if (!folder.exists) {
@@ -118,5 +294,43 @@ function toFolder(_name) {
   return folder;
 }
 
+/**
+ * Function to check whether an item exists in an array or strings.
+ * Cause ECMA-262 (1999) does not have .indexOf function on arrays. :-)
+ * @param {Array.<Object>} array array
+ * @param lookedUpString string being searched for in the array
+ * @param {string} [lookInNestedProperty] match property of a nested object in the array
+ * @returns {number} index of the string in array. Returns -1 if not present.
+ */
+function indexOf(array, lookedUpString, lookInNestedProperty) {
+  if (!!lookInNestedProperty) {
+    for (var i = 0; i < array.length; i++) {
+      if (lookedUpString === array[i][lookInNestedProperty]) {
+        return i;
+      }
+    }
+    return -1;
+  } else {
+    for (var j = 0; j < array.length; j++) {
+      if (lookedUpString === array[j]) {
+        return j;
+      }
+    }
+    return -1;
+  }
+}
+
+/**
+ * Get weight of the rarity from layer name.
+ * @param layerName name of the layer
+ * @returns {number} parsed weight of the layer, returns 1 if unset.
+ */
+function getRarityWeight(layerName) {
+  var weight = Number(layerName.split("#").pop());
+  if (isNaN(weight)) {
+    weight = 1;
+  }
+  return weight;
+}
 
 main();
